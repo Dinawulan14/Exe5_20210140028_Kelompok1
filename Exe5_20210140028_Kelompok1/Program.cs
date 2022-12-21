773d9305bb3ee72bcc2738bd39623838f63a9a42
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-12-21 16:04:13 +0700

    membuat public void insert

--- Exe5_20210140028_Kelompok1/Program.cs
+++ Exe5_20210140028_Kelompok1/Program.cs
@@ -19,6 +19,23 @@ namespace Exe5_20210140028_Kelompok1
             Dina = null;
             Sari = null;
         }
+        public void insert()
+        {
+            string valueread;
+            Node newnode = new Node();
+            Console.Write("Masukkan Input : ");
+            valueread = Console.ReadLine();
+            newnode.nama = valueread;
+            newnode.next = null;
+            if (Dina == null)
+            {
+                Dina = newnode;
+                Sari = newnode;
+                return;
+            }
+            Sari.next = newnode;
+
+        }
 
         }
     }
